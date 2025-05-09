3548935192208117f1e8ed3295ba82e21039ec8c
mepcteach <mi.puebla@profesor.duoc.cl>
2025-05-08 20:44:20 -0400

    Add files via upload

---
+++ virtualtrip/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for virtualtrip project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtualtrip.settings')
+
+application = get_wsgi_application()
